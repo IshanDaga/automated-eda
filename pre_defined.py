947db6249415c2880890bd2e4c1d16557a8614b7
Ishan Daga <ishan@hlth.dev>
2022-02-14 05:07:09 +0530

    added decision tree classifier

--- pre_defined.py
+++ pre_defined.py
@@ -1,19 +1,80 @@
 from sklearn import preprocessing
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
+import pandas as pd
+from sklearn.metrics import classification_report
 
 """
 A file of pre-defined functions for use in the project.
 Operations that can be generalised across different datasets and are reusable
 are defined in this file
 """
+# check if key exists in a list of dictionaries
+def key_exists(k1, k2, list_of_dicts):
+    for d in list_of_dicts:
+        if k1 == d['Column 1'] and k2 == d['Column 2'] or k1 == d['Column 2'] and k2 == d['Column 1']:
+            return True
+    return False
+
+# find columns with correlation greater than threshold value
+def get_good_correlation(df, threshold=0.15):
+    cor = df.corr()
+    good_cor = []
+    for i in cor.columns:
+        for j in cor.columns:
+            if i != j and (cor[i][j] > threshold or cor[i][j] < -threshold) and key_exists(i, j, good_cor) == False:
+                good_cor.append(
+                    {
+                        'Column 1': i,
+                        'Column 2': j,
+                        'cor': cor[i][j]
+                    }
+                )
+    return cor, good_cor
 
 #normalise numerical values and label encode non-numerical values
-def normalise_and_label_encode(data):
+def normalise_and_encode(data, data_y, **kwargs):
     for col in data.columns:
         if data[col].dtype == 'object':
-            le = preprocessing.LabelEncoder()
-            data[col] = le.fit_transform(data[col])
+            # if we are using a decision tree classifier, then label encode only the target column
+            if 'dct' in kwargs:
+                if col == data_y:
+                    le = preprocessing.LabelEncoder()
+                    data[col] = le.fit_transform(data[col])
+            # if not a dct, then label encode all non-numerical columns
+            else:
+                le = preprocessing.LabelEncoder()
+                data[col] = le.fit_transform(data[col])
+        # normalise numerical data         
         elif data[col].dtype != 'object':
             min_max_scaler = preprocessing.MinMaxScaler()
             x_scaled = min_max_scaler.fit_transform(data[col].values.reshape(-1, 1))
             data[col] = x_scaled
+
+    # one hot encode categorical data for decision tree classifier
+    if 'dct' in kwargs:
+        data = pd.get_dummies(data)
     return data
+
+"""
+Test - Train Split boilerplate
+"""
+def split_data(x,y):
+    x_train, x_test, y_train, y_test = train_test_split(x,y, test_size=0.3)
+    return x_train, x_test, y_train, y_test
+
+"""
+Decison Tree Classifier Function
+    simple decisions tree classifer
+    @returns: model, mean accuracy score
+"""
+def decision_tree_classifier(df, data_y):
+    x = df.drop(data_y, axis=1)
+    y = df[data_y]
+    x_train, x_test, y_train, y_test = split_data(x,y)
+    model_dt = DecisionTreeClassifier(criterion='gini',random_state=100,max_depth=6, min_samples_leaf=8)
+    model_dt.fit(x_train, y_train)
+    y_pred = model_dt.predict(x_test)
+    report = classification_report(y_test, y_pred, labels = [0,1], output_dict=True)
+
+    return model_dt, y_pred, model_dt.score(x_test,y_test), report
--- serve.py
+++ serve.py
@@ -1,35 +1,44 @@
+import base64
 import streamlit as st
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+from dtreeviz.trees import dtreeviz
+import streamlit.components.v1 as components
 import openai
-from pre_defined import normalise_and_label_encode
+from pre_defined import normalise_and_encode, get_good_correlation, decision_tree_classifier
 
 openai.api_key = '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'
 
-# check if key exists in a list of dictionaries
-def key_exists(k1, k2, list_of_dicts):
-    for d in list_of_dicts:
-        if k1 == d['Column 1'] and k2 == d['Column 2'] or k1 == d['Column 2'] and k2 == d['Column 1']:
-            return True
-    return False
+def general_eda(df, data_y):
+    # ask user for custom threshold values
+    threshold = st.slider("threshold", 0.0, 1.0, 0.4)
 
-# find columns with correlation greater than threshold value
-def get_good_correlation(df, threshold=0.15):
-    cor = df.corr()
-    good_cor = []
-    for i in cor.columns:
-        for j in cor.columns:
-            if i != j and (cor[i][j] > threshold or cor[i][j] < -threshold) and key_exists(i, j, good_cor) == False:
-                good_cor.append(
-                    {
-                        'Column 1': i,
-                        'Column 2': j,
-                        'cor': cor[i][j]
-                    }
-                )
-    return cor, good_cor
+    #get correlation and values for good correlation
+    cor, good_cor = get_good_correlation(df, threshold)
 
+    # plot correlation heatmap
+    fig, ax = plt.subplots()
+    # sns.heatmap(cor, annot=True, ax=ax)
+    st.pyplot(sns.pairplot(cor))
+
+    # print the columns with an acceptable correlation
+    st.markdown("## We reccomend using the following columns to create a model: ")
+    st.table(good_cor)
+    
+    if data_y:
+        already_plotted = []
+        for i in good_cor:
+            if i['Column 1'] != data_y and i['Column 1'] not in already_plotted:
+                fig, ax = plt.subplots()
+                sns.scatterplot(x = df[i['Column 1']], y = df[data_y], ax=ax)
+                already_plotted.append(i['Column 1'])
+                st.pyplot(fig)
+            if i['Column 2'] != data_y and i['Column 2'] not in already_plotted:
+                fig, ax = plt.subplots()
+                sns.scatterplot(x = df[i['Column 2']], y = df[data_y], ax=ax)
+                already_plotted.append(i['Column 2'])
+                st.pyplot(fig)
 
 #openai api function for data modeling
 def get_model(**kwargs):
@@ -44,6 +53,22 @@ def get_model(**kwargs):
         n=1
     )
     return response['choices'][0]['text']
+
+#decision tree visulaization
+def svg_write(svg, center=True):
+    """
+    Disable center to left-margin align like other objects.
+    """
+    # Encode as base 64
+    b64 = base64.b64encode(svg.encode("utf-8")).decode("utf-8")
+
+    # Add some CSS on top
+    css_justify = "center" if center else "left"
+    css = f'<p style="text-align:center; display: flex; justify-content: {css_justify};">'
+    html = f'{css}<img src="data:image/svg+xml;base64,{b64}"/>'
+
+    # Write the HTML
+    st.write(html, unsafe_allow_html=True)
 
 def main():
     st.set_page_config(page_title='Automated EDA and ML',
@@ -65,52 +90,52 @@ def main():
         # output all columns in dataframe
         st.write(columns, scrolling=True)
 
-        df = normalise_and_label_encode(df)
-
-        # get column to be predicted in our model
-        st.subheader("What column do you want to predict?", columns)
+        st.subheader("What is your column of interest?", columns)
         data_y = st.selectbox('', options=columns)
+
+        radio_option = st.radio('What do you want to do?', ('EDA', 'Model'))
+
+        if radio_option == 'EDA':
+            general_eda(df, data_y)
+        elif radio_option == 'Model':
+            radio_option = st.radio('What model do you want to use?', ('Decision Tree', 'Linear Regression', 'Random Forest', 'Logistic Regression'))
+            if radio_option == 'Decision Tree':
+                df = normalise_and_encode(df, data_y, dct=True)
+                model, predicted, score, report = decision_tree_classifier(df, data_y=data_y)
+                st.write(f'The model score is {score}')
+                st.subheader('Model Report')
+                st.table(report)   
+                viz = dtreeviz(
+                        model,
+                        x_data = df.drop(data_y, axis=1),
+                        y_data = df[data_y],
+                        target_name = data_y,
+                        feature_names = df.drop(data_y, axis=1).columns,
+                        class_names = ['Churned', 'Not Churned'],
+                    )
+                svg_write(viz.svg())
+                       
+            elif radio_option == 'Linear Regression':
+                options = st.multiselect('Select columns to model', columns)
+                st.write('You selected:', options)
+                pass
+            elif radio_option == 'Random Forest':
+                pass
+            elif radio_option == 'Logistic Regression':
+                pass
         
-        # ask user for custom threshold values
-        threshold = st.slider("threshold", 0.0, 1.0, 0.4)
 
-        #get correlation and values for good correlation
-        cor, good_cor = get_good_correlation(df, threshold)
-
-        # plot correlation heatmap
-        fig, ax = plt.subplots()
-        sns.heatmap(cor, annot=True, ax=ax)
-
-        # print the columns with an acceptable correlation
-        st.markdown("## We reccomend using the following columns to create a model: ")
-        st.table(good_cor)
         
-        if data_y:
-            already_plotted = []
-            for i in good_cor:
-                if i['Column 1'] != data_y and i['Column 1'] not in already_plotted:
-                    fig, ax = plt.subplots()
-                    sns.scatterplot(x = df[i['Column 1']], y = df[data_y], ax=ax)
-                    already_plotted.append(i['Column 1'])
-                    st.pyplot(fig)
-                if i['Column 2'] != data_y and i['Column 2'] not in already_plotted:
-                    fig, ax = plt.subplots()
-                    sns.scatterplot(x = df[i['Column 2']], y = df[data_y], ax=ax)
-                    already_plotted.append(i['Column 2'])
-                    st.pyplot(fig)
 
-        options = st.multiselect('Select columns to model', columns)
-        st.write('You selected:', options)
-
-        if options:
-            st.subheader("Model from OpenAI inference")
-            if st.button('Get Code'):
-                with st.spinner('Wait for it...'):
-                    code = get_model(data_x=options, data_y=data_y)
-                st.success('Done!')
-                st.code(code, language='python')
-                # run code
-                if st.button('Run Code'):
-                    st.write(exec(code))
+        # if options:
+        #     st.subheader("Model from OpenAI inference")
+        #     if st.button('Get Code'):
+        #         with st.spinner('Wait for it...'):
+        #             code = get_model(data_x=options, data_y=data_y)
+        #         st.success('Done!')
+        #         st.code(code, language='python')
+        #         # run code
+        #         if st.button('Run Code'):
+        #             st.write(exec(code))
 if __name__ == '__main__':
     main()
